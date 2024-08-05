794185b172fe4a5bc6b30a37da5d2894804f7239
MOUDILA <moudilamarcel@gmail.com>
2024-08-06 01:18:33 +0200

    Amélioration de l'affichage du tableau de bord Power BI

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,10 @@
-document.addEventListener('DOMContentLoaded', function() {
-    console.log('Le fichier JavaScript est bien chargé.');
+// scripts.js
+
+// Ajoutez des interactions JavaScript si nécessaire
+
+// Exemple : Afficher une alerte lorsque l'iframe est cliqué
+document.querySelectorAll('.project iframe').forEach(function(iframe) {
+    iframe.addEventListener('click', function() {
+        alert('Vous avez cliqué sur le tableau de bord Power BI');
+    });
 });
